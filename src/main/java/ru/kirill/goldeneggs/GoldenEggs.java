839c64c26d67e37283c47c0db9f228fc9d952524
DarkClientFT <nacekirill@yandex.ru>
2026-06-04 19:15:30 +0300

    Add project source files

---
+++ src/main/java/ru/kirill/goldeneggs/GoldenEggs.java
@@ -0,0 +1,17 @@
+package ru.kirill.goldeneggs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GoldenEggs extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
